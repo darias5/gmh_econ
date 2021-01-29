d7a9faa054e9c1c5fcfaf788fa80f05419baf951
Daniel Arias <darias@g.harvard.edu>
2021-01-29 09:06:41 -0500

    Updating code

--- code/dofile.R
+++ code/dofile.R
@@ -1,10 +1,17 @@
+##################################################################
+#
+# Re-evaluating the Global Economic Burden of Mental Illness
+# Daniel Arias, PhD Student, Population Health Sciences
+# November 23, 2020
+#
+##################################################################
 
 ######################
 ##   HOUSEKEEPING   ##
 ######################
 
-# setting directories
-path <- "~/3. PhD/WQE/WQE II/gbd_mh"
+# setting directories - change to your local directory
+path <- "~/3. PhD/WQE/WQE II/gmh_cost"
 datapath <- paste(path,'data', sep = "/")
 
 # Loading libraries
@@ -22,13 +29,13 @@ library(maps)
 
 
 # Loading IHME Global Burden of Disease Data
-data <- read.csv(file = file.path(datapath,"IHME-GBD_2019_DATA-4aa992a4-1.csv"))
+data <- read.csv(file = file.path(datapath,"IHME-GBD_2019_DATA-2019-1.csv"))
 
 # Adding alpha-3 codes from ISO 3166-1 (i.e., World Bank Codes)
 ihme_crosswalk <- read_excel(path = file.path(datapath, "ihme_crosswalk.xlsx")) %>% 
   select(-c("location_name"))
 
-data <- full_join(data, ihme_crosswalk, by = "location_id")
+data <- left_join(data, ihme_crosswalk, by = "location_id")
 
 # clean up
 rm(ihme_crosswalk) 
@@ -38,33 +45,21 @@ income_level <- read_excel(path = file.path(datapath, "OGHIST.xls"), sheet = 2) 
   select(-(c(2:34))) %>% 
   rename("income_level" = "2019")
 
-data <- full_join(data, income_level, by = "iso_code")
+data <- left_join(data, income_level, by = "iso_code")
 
 # clean up
 rm(income_level) 
 
 # Adding population
-population <- read.csv(file = file.path(datapath, "population.csv")) %>%  
-  select((c(2, 3, 23))) %>% 
-  filter(indicator_name == "Population, total")%>% 
-  select(-(c(2)))  %>% 
-  rename("population" = "X2019")
-population$population <- as.character(population$population)
-population$population <- as.numeric(population$population)
-population$population[population$iso_code == "ERI"] <- "6711213" 
+data <- data %>% unique() %>% gather("numeric_name", "est", "val":"lower") 
+data_number <- data %>% filter(metric_id == 1) %>% rename("number" = "est")
+data_rate <- data %>% filter(metric_id == 3) 
+data_rate <- data %>% select("measure_id","location_id", "sex_id", "age_id", "cause_id", "year", "numeric_name", "est") %>% rename("rate" = "est")
+data <- left_join(data_number, data_rate, by = c("measure_id","location_id", "sex_id", "age_id", "cause_id", "year", "numeric_name"))
+rm(data_number, data_rate)
 
-#http://ghdx.healthdata.org/geography/eritrea?page=1
-
-
-
-data <- full_join(data, population, by = "iso_code")
-data$population <- as.numeric(data$population)
-
-# clean up
-rm(population) 
-
-# Adding rates
-data <- data %>%  mutate(rate = val/population)
+data <- data %>% unique() %>% filter(numeric_name == "val") %>% spread(numeric_name, number)
+data$population <- data$val/data$rate
 
 ######################
 ## DATA EXPLORATION ##
@@ -81,7 +76,7 @@ rm(world_map_crosswalk)
 # Subset of data
 data_selfharm_dalys <- data %>% filter(cause_name == "Self-harm") %>%
   filter(metric_id == "1") %>%
-  filter(measure_id== "2") %>% select(c(14,17, 20))
+  filter(measure_id== "2") %>% select(c(14,16, 17))
 
 world_map <- inner_join(world_map, data_selfharm_dalys, by = "iso_code")
 rm(data_selfharm_dalys)
@@ -105,7 +100,7 @@ world_map %>%
 # DALYs due to self-harm, rate
 
 world_map %>% 
-  ggplot(aes(x = long, y = lat, group = group, fill = rate*100000)) +
+  ggplot(aes(x = long, y = lat, group = group, fill = rate)) +
   geom_polygon(color = "black", size = 0.01) + 
   theme(panel.grid.major = element_blank(), 
         panel.background = element_blank(),
@@ -121,11 +116,6 @@ world_map %>%
 ############################
 ## vIGO ET AL REPLICATION ##
 ############################
-
-####### Load 2013 data
-
-# Loading IHME Global Burden of Disease Data
-data_2019 <- read.csv(file = file.path(datapath,"IHME-GBD_2019_DATA-2013.csv"))
 
 # VIGO METHOD: ALLOCATION OF THE FOLLOWING TO MENTAL HEALTH (PERCENTS DENOTE AMOUNT REALLOCATED):
 
@@ -153,42 +143,16 @@ revision_full <- c(mental_disorder_cause_id,
               self_harm)
 
 revision_third <- musculoskeletal_cause_id
+inclusion <- c(mental_disorder_cause_id,
+               dementia_cause_id,
+               epilepsy_cause_id,
+               migraine_cause_id,
+               tension_type_headache_cause_id,
+               self_harm,
+               musculoskeletal_cause_id,
+               all_cause_id)
 
-data_2019$mh_gbd <- ifelse(data_2019$cause == mental_disorder_cause_id, 1, 0)
-data_2019$mh_vigo <- ifelse(data_2019$cause %in% revision_full, 1, 0)
-data_2019$mh_vigo[data_2019$cause %in% revision_third] <- 1/3
-data_2019$all <- ifelse(data_2019$cause == all_cause_id, 1, 0)
-data_2019$value <- data_2019$val * data_2019$mh_gbd
-data_2019$value_rev <- data_2019$val * data_2019$mh_vigo
-data_2019 <- data_2019 %>% group_by(location, measure, sex, age, metric) %>% mutate(mh_value_agg_rev = sum(value_rev), mh_value_agg = sum(value))
-
-data_2019_global <- data_2019 %>% filter(location == 1) %>% unique()
-
-global_2019_dalys <- data_2019_global$val[data_2019_global$cause == 294 & data_2019_global$measure == 2]
-global_2019_ylls <- data_2019_global$val[data_2019_global$cause == 294 & data_2019_global$measure == 3]
-
-global_2019_dalys_mh_orig <- data_2019_global$mh_value_agg[data_2019_global$cause == 294 & data_2019_global$measure == 2]
-global_2019_ylls_mh_orig <- data_2019_global$mh_value_agg[data_2019_global$cause == 294 & data_2019_global$measure == 3]
-
-# Testing: all.equal tests should yield roughly equivalent results
-# Source: http://ghdx.healthdata.org/gbd-results-tool
-
-all.equal(115674919, global_2019_dalys_mh_orig)
-all.equal(115658862, global_2019_ylls_mh_orig)
-all.equal(0.0455 , global_2019_dalys_mh_orig/global_2019_dalys)
-all.equal(0.1494 , global_2019_ylls_mh_orig/global_2019_ylls)
-
-# global_2019_dalys_mh_vigo <- data_2019_global$mh_value_agg_rev[data_2019_global$cause == 294 & data_2019_global$measure == 2]
-# global_2019_ylls_mh_vigo <- data_2019_global$mh_value_agg_rev[data_2019_global$cause == 294 & data_2019_global$measure == 3]
-
-# global_2019_dalys_mh_vigo
-# global_2019_ylls_mh_vigo
-
-# global_2019_dalys_mh_vigo/global_2019_dalys
-# global_2019_ylls_mh_vigo/global_2019_ylls
-
-
-# 
+data <- data %>% filter(cause_id %in% inclusion)
 
 data$mh_gbd <- ifelse(data$cause_id == mental_disorder_cause_id, 1, 0)
 data$mh_vigo <- ifelse(data$cause_id %in% revision_full, 1, 0)
@@ -201,30 +165,30 @@ data <- data %>% group_by(location_id, measure_id, sex_id, age_id, metric_id) %>
 rm(all_cause_id, dementia_cause_id, epilepsy_cause_id, mental_disorder_cause_id, migraine_cause_id, musculoskeletal_cause_id, 
    revision_full, revision_third, self_harm, tension_type_headache_cause_id)
 
-data_global <- data %>% filter(location_id == 1) %>% unique()
+data_global <- data %>% filter(location_id == 1) %>% filter(population != 1) %>% filter(metric_id == 1) %>% filter(cause_id == 294) %>% unique()
 
-global_2019_dalys <- data_global$val[data_global$cause_id == 294 & data_global$measure_id == 2]
-global_2019_ylls <- data_global$val[data_global$cause_id == 294 & data_global$measure_id == 3]
+global_2019_dalys <- data_global$val[data_global$measure_id == 2]
+global_2019_ylds <- data_global$val[data_global$measure_id == 3]
 
-global_2019_dalys_mh_orig <- data_global$mh_value_agg[data_global$cause_id == 294 & data_global$measure_id == 2]
-global_2019_ylls_mh_orig <- data_global$mh_value_agg[data_global$cause_id == 294 & data_global$measure_id == 3]
+global_2019_dalys_mh_orig <- data_global$mh_value_agg[data_global$measure_id == 2]
+global_2019_ylds_mh_orig <- data_global$mh_value_agg[data_global$measure_id == 3]
 
 # Testing: all.equal tests should yield roughly equivalent results
 # Source: http://ghdx.healthdata.org/gbd-results-tool
 
-all.equal(115674919, global_2019_dalys_mh_orig)
-all.equal(115658862, global_2019_ylls_mh_orig)
+all.equal(125311322, global_2019_dalys_mh_orig)
+all.equal(125293960, global_2019_ylds_mh_orig)
 all.equal(0.0455 , global_2019_dalys_mh_orig/global_2019_dalys)
-all.equal(0.1494 , global_2019_ylls_mh_orig/global_2019_ylls)
+all.equal(0.1494 , global_2019_ylds_mh_orig/global_2019_ylds)
 
-global_2019_dalys_mh_vigo <- data_2019_global$mh_value_agg_rev[data_2019_global$cause_id == 294 & data_2019_global$measure_id == 2]
-global_2019_ylls_mh_vigo <- data_2019_global$mh_value_agg_rev[data_2019_global$cause_id == 294 & data_2019_global$measure_id == 3]
+global_2019_dalys_mh_vigo <- data_global$mh_value_agg_rev[data_global$measure_id == 2]
+global_2019_ylds_mh_vigo <- data_global$mh_value_agg_rev[data_global$measure_id == 3]
 
 global_2019_dalys_mh_vigo
-global_2019_ylls_mh_vigo
+global_2019_ylds_mh_vigo
 
 global_2019_dalys_mh_vigo/global_2019_dalys
-global_2019_ylls_mh_vigo/global_2019_ylls
+global_2019_ylds_mh_vigo/global_2019_ylds
 
 
 ######################
@@ -234,14 +198,15 @@ global_2019_ylls_mh_vigo/global_2019_ylls
 
 # Subset of data
 data_gh <- data %>%
-  filter(cause_id == "294") %>%
-  filter(metric_id == "1") %>%
-  select(c(1, 14,17, 19, 26, 27)) 
+  filter(cause_id == "294") %>% #Filters to all causes; data_gh$val == total burden by location id
+  filter(metric_id == "1") %>%  #Filters to numeric metric
+  select(c(1, 14:18, 22:25)) 
 
-data_gh <- data_gh %>% rename("Revised" = mh_value_agg_rev, "Original" = mh_value_agg)
-data_gh <- data_gh %>% ungroup() %>% select(c(5:10))
-data_gh_long <- data_gh %>% gather("estimate", "number", Revised:Original)
-data_gh_long$rate <- data_gh_long$number/data_gh_long$population*100000
+data_gh <- data_gh %>% rename("Revised - Vigo et al. method" = mh_value_agg_rev, "Original" = mh_value_agg)
+
+data_gh <- data_gh %>% ungroup() %>% select(c(5:14))
+data_gh_long <- data_gh %>% gather("estimate", "number", "Revised - Vigo et al. method":"Original")
+data_gh_long$rate_100k <- data_gh_long$number/data_gh_long$population
 data_gh_long$percent <- data_gh_long$number/data_gh_long$val*100
 
 world_map = map_data("world")
@@ -250,13 +215,33 @@ world_map <- inner_join(world_map, world_map_crosswalk, by = "region")
 world_map <- world_map %>% filter(iso_code != "..") %>% select(!(c(6)))
 
 rm(world_map_crosswalk)
-
+data_gh_long <- data_gh_long %>% filter(population != 1) %>% select(iso_code, measure_id, number, percent, rate_100k, estimate)
 world_map <- inner_join(world_map, data_gh_long, by = "iso_code") 
+
 rm(data_gh, data_gh_long)
+
+# Rates
+
+caption <- "Source: Global Burden of Disease Study, Vigo et al. 2016"
+
+world_map %>% filter(measure_id== "1") %>% 
+  ggplot(aes(x = long, y = lat, group = group)) +
+  geom_polygon(aes(fill = rate_100k), color = "black", size = 0.01) + 
+  theme(panel.grid.major = element_blank(), 
+        panel.background = element_blank(),
+        axis.title = element_blank(), 
+        axis.text = element_blank(),
+        axis.ticks = element_blank()) +
+  labs(title="Deaths due to mental disorders per 100,000",
+       subtitle="2019",
+       caption=caption,
+       fill="Deaths") +
+  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$rate[world_map$measure_id== "1"]))) +
+  facet_grid(~estimate)
 
 world_map %>% filter(measure_id== "2") %>% 
   ggplot(aes(x = long, y = lat, group = group)) +
-  geom_polygon(aes(fill = rate), color = "black", size = 0.01) + 
+  geom_polygon(aes(fill = rate_100k), color = "black", size = 0.01) + 
   theme(panel.grid.major = element_blank(), 
         panel.background = element_blank(),
         axis.title = element_blank(), 
@@ -264,14 +249,14 @@ world_map %>% filter(measure_id== "2") %>%
         axis.ticks = element_blank()) +
   labs(title="Disability-adjusted life years due to mental disorders per 100,000",
        subtitle="2019",
-       caption="Source: Global Burden of Disease Study",
+       caption=caption,
        fill="DALYs") +
-  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$rate))) +
+  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$rate[world_map$measure_id== "2"]))) +
   facet_grid(~estimate)
 
 world_map %>% filter(measure_id== "3") %>% 
   ggplot(aes(x = long, y = lat, group = group)) +
-  geom_polygon(aes(fill = rate), color = "black", size = 0.01) + 
+  geom_polygon(aes(fill = rate_100k), color = "black", size = 0.01) + 
   theme(panel.grid.major = element_blank(), 
         panel.background = element_blank(),
         axis.title = element_blank(), 
@@ -279,12 +264,42 @@ world_map %>% filter(measure_id== "3") %>%
         axis.ticks = element_blank()) +
   labs(title="Years lived with disability due to mental disorders per 100,000",
        subtitle="2019",
-       caption="Source: Global Burden of Disease Study",
+       caption=caption,
        fill="YLDs ") +
-  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$rate))) +
+  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$rate[world_map$measure_id== "3"]))) +
   facet_grid(~estimate)
 
+world_map %>% filter(measure_id== "4") %>% 
+  ggplot(aes(x = long, y = lat, group = group)) +
+  geom_polygon(aes(fill = rate_100k), color = "black", size = 0.01) + 
+  theme(panel.grid.major = element_blank(), 
+        panel.background = element_blank(),
+        axis.title = element_blank(), 
+        axis.text = element_blank(),
+        axis.ticks = element_blank()) +
+  labs(title="Years of life lost due to mental disorders per 100,000",
+       subtitle="2019",
+       caption=caption,
+       fill="YLLs ") +
+  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$rate[world_map$measure_id== "4"]))) +
+  facet_grid(~estimate)
 
+# Percents of burden
+
+world_map %>% filter(measure_id== "1") %>% 
+  ggplot(aes(x = long, y = lat, group = group)) +
+  geom_polygon(aes(fill = percent), color = "black", size = 0.01) + 
+  theme(panel.grid.major = element_blank(), 
+        panel.background = element_blank(),
+        axis.title = element_blank(), 
+        axis.text = element_blank(),
+        axis.ticks = element_blank()) +
+  labs(title="Deaths due to mental disorders",
+       subtitle="2019",
+       caption=caption,
+       fill="% of deaths, total") +
+  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$percent[world_map$measure_id== "1"]))) +
+  facet_grid(~estimate)
 
 world_map %>% filter(measure_id== "2") %>% 
   ggplot(aes(x = long, y = lat, group = group)) +
@@ -296,9 +311,9 @@ world_map %>% filter(measure_id== "2") %>%
         axis.ticks = element_blank()) +
   labs(title="Disability-adjusted life years due to mental disorders",
        subtitle="2019",
-       caption="Source: Global Burden of Disease Study",
+       caption=caption,
        fill="% of DALYs, total") +
-  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$percent))) +
+  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$percent[world_map$measure_id== "2"]))) +
   facet_grid(~estimate)
 
 world_map %>% filter(measure_id== "3") %>% 
@@ -311,20 +326,32 @@ world_map %>% filter(measure_id== "3") %>%
         axis.ticks = element_blank()) +
   labs(title="Years lived with disability due to mental disorders",
        subtitle="2019",
-       caption="Source: Global Burden of Disease Study",
+       caption=caption,
        fill="% of YLDs, total ") +
-  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$percent))) +
+  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$percent[world_map$measure_id== "3"]))) +
+  facet_grid(~estimate)
+
+world_map %>% filter(measure_id== "4") %>% 
+  ggplot(aes(x = long, y = lat, group = group)) +
+  geom_polygon(aes(fill = percent), color = "black", size = 0.01) + 
+  theme(panel.grid.major = element_blank(), 
+        panel.background = element_blank(),
+        axis.title = element_blank(), 
+        axis.text = element_blank(),
+        axis.ticks = element_blank()) +
+  labs(title="Years of life lost due to mental disorders",
+       subtitle="2019",
+       caption=caption,
+       fill="% of YLLs, total ") +
+  scale_fill_distiller(palette = "RdYlBu", limits = c(0,max(world_map$percent[world_map$measure_id== "4"]))) +
   facet_grid(~estimate)
 
 
 ## To Do
-# Redownload data to include 
-# Fix scales
-# Add all causes and all locations line for totals
-# Do not include percents or rates
+# Walker alternative -- use percent attributable risk to calculate 
+# Check changes in rankings
 # Look up if there have been changes in disability weights
 # Critiques of disability weights 
-# Consider looking at deaths - especially suicide
 
 
 
